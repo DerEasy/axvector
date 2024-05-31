c7226b1c17d1604c0320007c09cd02d89e0066c3
DerEasy <alstaden.ismail@gmail.com>
2024-05-31 23:46:24 +0200

    added licence headers

--- axvector.c
+++ axvector.c
@@ -2,6 +2,12 @@
 // Created by easy on 27.10.23.
 //
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
--- axvector.h
+++ axvector.h
@@ -2,6 +2,12 @@
 // Created by easy on 27.10.23.
 //
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef AXVECTOR_AXVECTOR_H
 #define AXVECTOR_AXVECTOR_H
 
